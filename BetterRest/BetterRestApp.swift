4b63713469d1a222e073847baba8d9bb2c136fb7
Juliette Rapala <jrapala@gmail.com>
2020-09-30 16:54:14 -0500

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Juliette Rapala on 9/30/20.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
